28e208f77d987f5159b951b153d606b8474b24ae
Kaustubh <147119055+kaustubh-ds@users.noreply.github.com>
2023-10-10 10:20:23 +0530

    Add files via upload

---
+++ Word Scramble Puzzle/js/words.js
@@ -0,0 +1,94 @@
+let words = [
+    {
+        word: "excessive",
+        hint: "More than necessary"
+    },
+    {
+        word: "travel",
+        hint: "Going from one place to another"
+    },
+    {
+        word: "orange",
+        hint: "Red + yellow"
+    },
+    {
+        word: "october",
+        hint: "10th month of year"
+    },
+    {
+        word: "seven",
+        hint: "Wonders of nature"
+    },
+    {
+        word: "garden",
+        hint: "Space for planting flower and plant"
+    },
+    {
+        word: "ability",
+        hint: "A skill to do a particular thing"
+    },
+    {
+        word: "door",
+        hint: "Thing you open when your hear a knock on it"
+    },
+    {
+        word: "english",
+        hint: "International language"
+    },
+    {
+        word: "mathematics",
+        hint: "Subject known as the hardest one among young students"
+    },
+    {
+        word: "expansion",
+        hint: "The process of increase or grow"
+    },
+    {
+        word: "country",
+        hint: "A politically identified region"
+    },
+    {
+        word: "group",
+        hint: "A number of objects or persons"
+    },
+    {
+        word: "taste",
+        hint: "Ability of tongue to detect flavour"
+    },
+    {
+        word: "store",
+        hint: "Large shop where goods are traded"
+    },
+    {
+        word: "field",
+        hint: "Area of land for farming activities"
+    },
+    {
+        word: "friend",
+        hint: "Person other than a family member"
+    },
+    {
+        word: "pocket",
+        hint: "A bag for carrying small items"
+    },
+    {
+        word: "needle",
+        hint: "A thin and sharp metal pin"
+    },
+    {
+        word: "expert",
+        hint: "Person with extensive knowledge"
+    },
+    {
+        word: "statement",
+        hint: "A declaration of something"
+    },
+    {
+        word: "second",
+        hint: "One-sixtieth of a minute"
+    },
+    {
+        word: "library",
+        hint: "Place containing collection of books"
+    },
+]
